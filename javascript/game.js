a8af46a212d132015d591f07a2b1e7281144ffbc
Bianca <canisenelson@gmail.com>
2019-09-04 20:31:56 -0400

    Crystals-Collector-Game

---
+++ javascript/game.js
@@ -0,0 +1,10 @@
+$ ( document ).ready(function(){
+
+var Random=Math.floor(Math.random()*102+19)
+
+$('#targetNum').text(Random);
+
+var crystals= Math.floor(Math.random()*12+1;
+console.log(crystals)
+
+}
