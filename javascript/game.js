965f6d856b12bb253790bd4d39196b1a8137a5ba
Bianca <canisenelson@gmail.com>
2019-09-15 05:46:08 -0400

    Crystals-Collector-Game

--- javascript/game.js
+++ javascript/game.js
@@ -1,88 +1,36 @@
-// $( document ).ready(function(){
-var Random = Math.floor(Math.random() * 102 + 19)
-
-$("#target").text(Random);
-console.log(Random)
-
-var crys4 = Math.floor(Math.random()*12+1)
-var crys2 = Math.floor(Math.random()*12+1)
-var crys3 = Math.floor(Math.random()*12+1)
-var crys1 = Math.floor(Math.random()*12+1)
-
-
 var totalScore = 0;
 var wins = 0;
 var losses = 0;
+var random = Math.floor(Math.random() * 102 + 19);
 
-$("#wins").text(wins);
-$("#losses").text(losses);
+//TODO: Your image paths go in this array as strings.
+var images = ["./assests/images/crystalFour.jpg","./assests/images/crystalTwo.jpg", "./assests/images/crystalThree.jpg", "./assests/images/crystalOne.jpg"];
 
-function reset() {
-    Random = Math.floor(Math.random() * 102 + 19);
+$('#target').text(random);
+$('#wins').text(wins);
+$('#losses').text(losses);
 
-    $("#target").text(Random);
-    crys4 = Math.floor(Math.random()*12+1);
-    crys2 = Math.floor(Math.random()*12+1);
-    crys3 = Math.floor(Math.random()*12+1);
-    crys1 = Math.floor(Math.random()*12+1);
-    totalScore = 0;
-    $("totalScore").text(totalScore);
-
-}
-function winner() {
-    alert("Winner Winner Chicken Dinner!");
-    wins++;
-    $('#wins').text(wins);
-    reset();
+// How to dynamically create crystals, based on the amount of images in your array.
+function generateCrystals (){
+	for (var i = 0; i < images.length; i++) {
+		var crystal = $('<img>');
+		crystal.attr('src', images[i]);
+		crystal.attr('data-crystal-value', Math.floor(Math.random() * 12 + 1));
+		crystal.addClass('crystal');
+		$('#content').append(crystal);
+	}
 }
 
-function loser() {
-    alert("Gameover!");
-    losses++;
-    $('#losses').text(losses);
-    reset()
-}
+generateCrystals();
 
-$(crys4).on('.click', function () {
-    totalScore = totalScore + crys4;
-    $('#totalScore').text(totalScore);
-    if (totalScore == Random) {
-        winner();
-    }
-    else if (totalScore > Random)
-        loser();
-})
-
-$(crys2).on('.click', function () {
-    totalScore = totalScore + crys2;
-    $('#totalScore').text(totalScore);
-    if (totalScore == Random) {
-        winner();
-    }
-    else if (totalScore > Random)
-        loser();
-})
-
-$(crys3).on('.click', function () {
-    totalScore = totalScore + crys3;
-    $('#totalScore').text(totalScore);
-    if (totalScore == Random) {
-        winner();
-    }
-    else if (totalScore > Random)
-        loser();
-})
-
-$(crys1).on('.click', function () {
-    totalScore = totalScore + crys1;
-    $('#totalScore').text(totalScore);
-    if (totalScore == Random) {
-        winner();
-    }
-    else if ( totalScore > Random)
-        loser();
-})
-
-function crystals(){
-    return crystals;
-};
+$(document).on('click', '.crystal', function (){
+	var crystalValue = parseInt($('.crystal').attr('data-crystal-value'));
+	totalScore += crystalValue;
+	$('#totalScore').text(totalScore);
+	if (totalScore === random) {
+		wins++;
+	}
+	else if (totalScore > random) {
+		losses++;
+	}
+});
