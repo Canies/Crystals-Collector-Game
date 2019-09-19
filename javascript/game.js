7c629ee504bd7537d116d45b841b307b5bb664d9
Bianca <canisenelson@gmail.com>
2019-09-19 04:27:44 -0400

    Crystals-Collector-Game

--- javascript/game.js
+++ javascript/game.js
@@ -2,16 +2,17 @@ var totalScore = 0;
 var wins = 0;
 var losses = 0;
 var random = Math.floor(Math.random() * 102 + 19);
+console.log(random)
 
 //TODO: Your image paths go in this array as strings.
-var images = ["./assests/images/crystalFour.jpg","./assests/images/crystalTwo.jpg", "./assests/images/crystalThree.jpg", "./assests/images/crystalOne.jpg"];
+var images = ["./assests/images/crystalFour.jpg", "./assests/images/crystalTwo.jpg", "./assests/images/crystalThree.jpg", "./assests/images/crystalOne.jpg"];
 
 $('#target').text(random);
 $('#wins').text(wins);
 $('#losses').text(losses);
 
 // How to dynamically create crystals, based on the amount of images in your array.
-function generateCrystals (){
+function generateCrystals() {
 	for (var i = 0; i < images.length; i++) {
 		var crystal = $('<img>');
 		crystal.attr('src', images[i]);
@@ -21,16 +22,37 @@ function generateCrystals (){
 	}
 }
 
+function reset() {
+	totalScore = 0;
+	$('#totalScore').text(totalScore);
+	random = Math.floor(Math.random() * 102 + 19);
+	$('#target').text(random);
+
+}
 generateCrystals();
 
-$(document).on('click', '.crystal', function (){
-	var crystalValue = parseInt($('.crystal').attr('data-crystal-value'));
+$(document).on('click', '.crystal', function () {
+	
+	var crystalValue = parseInt($(this).attr('data-crystal-value'));
+	console.log(crystalValue)
 	totalScore += crystalValue;
+	console.log(totalScore);
 	$('#totalScore').text(totalScore);
-	if (totalScore === random) {
-		wins++;
-	}
-	else if (totalScore > random) {
+	console.log(totalScore, random)
+	if (totalScore > random) {
+		console.log(totalScore, random)
+		// HTMLFormControlsCollection.log("Loss")
+		alert("Bad Math Buddy!")
 		losses++;
+		reset()
+
+	}
+	console.log(totalScore, random);
+	if ((totalScore == random) && (totalScore!=0)) {
+		console.log(totalScore, random);
+		alert("Congrats, You can Add!")
+		wins++;
+		reset()
+
 	}
 });
